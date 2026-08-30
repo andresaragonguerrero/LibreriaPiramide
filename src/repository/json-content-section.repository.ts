4bb029a3ff1febcad4ee7c0ed03d30244948df6a
andresaragonguerrero <andresaragonguerrero@gmail.com>
2026-08-30 22:50:44 +0200

    Corrección de errores

--- src/repository/json-content-section.repository.ts
+++ src/repository/json-content-section.repository.ts
@@ -1,13 +1,14 @@
 import sections from '@/data/content-sections.json'
 
-import type { ContentSection } from '@/types/content-section'
+import type { ContentSection, ContentSectionType } from '@/types/content-section'
 import type { ContentSectionRepository } from './content-section.repository'
 
 export class JsonContentSectionRepository implements ContentSectionRepository {
     async getSections(): Promise<ContentSection[]> {
         return sections.map(section => ({
             ...section,
-            books: []
+            type: section.type as ContentSectionType,
+            items: []
         })) as ContentSection[]
     }
 }
